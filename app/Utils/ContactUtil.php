93a2cdd10c2086913e81cca26bb4b294c919ab3e
TaskFlow Developer <developer@taskflow.app>
2026-02-01 12:16:20 +0200

    Define new method to get contact by mobile

--- app/Utils/ContactUtil.php
+++ app/Utils/ContactUtil.php
@@ -87,6 +87,31 @@ class ContactUtil extends Util
         return $contact;
     }
 
+    /**
+     * Returns the contact info
+     *
+     * @param  int  $business_id
+     * @param  int  $contact_id
+     * @return array
+     */
+    public function getContactInfoByMobile($business_id, $mobile)
+    {
+        $contact = Contact::where('contacts.mobile', $mobile)
+                    ->where('contacts.business_id', $business_id)
+                    ->leftjoin('transactions AS t', 'contacts.id', '=', 't.contact_id')
+                    ->with(['business'])
+                    ->select(
+                        DB::raw("SUM(IF(t.type = 'purchase', final_total, 0)) as total_purchase"),
+                        DB::raw("SUM(IF(t.type = 'sell' AND t.status = 'final', final_total, 0)) as total_invoice"),
+                        DB::raw("SUM(IF(t.type = 'purchase', (SELECT SUM(amount) FROM transaction_payments WHERE transaction_payments.transaction_id=t.id), 0)) as purchase_paid"),
+                        DB::raw("SUM(IF(t.type = 'sell' AND t.status = 'final', (SELECT SUM(IF(is_return = 1,-1*amount,amount)) FROM transaction_payments WHERE transaction_payments.transaction_id=t.id), 0)) as invoice_received"),
+                        DB::raw("SUM(IF(t.type = 'opening_balance', final_total, 0)) as opening_balance"),
+                        DB::raw("SUM(IF(t.type = 'opening_balance', (SELECT SUM(amount) FROM transaction_payments WHERE transaction_payments.transaction_id=t.id), 0)) as opening_balance_paid"),
+                        'contacts.*'
+                    )->first();
+
+        return $contact;
+    }
     public function createNewContact($input)
     {
         //Check Contact id
